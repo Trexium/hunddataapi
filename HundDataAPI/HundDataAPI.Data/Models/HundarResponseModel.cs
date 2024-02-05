e69de4fa48af05f05c9c8cd4c00fec364c025d83
trexium <emil@gilljam.org>
2024-02-05 22:02:16 +0100

    Started work on getting data from skk.

---
+++ HundDataAPI/HundDataAPI.Data/Models/HundarResponseModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace HundDataAPI.Data.Models
+{
+    public class HundarResponseModel
+    {
+        [JsonPropertyName("d")]
+        public Hund[] Dogs { get; set; }
+    }
+}
